371975525486f972a1a118bd24ed1b2f2f2ea89f
Ben Talker <107852275+inewthinker@users.noreply.github.com>
2022-10-19 11:23:03 +0300

    Update multijob.groovy

--- basics/dsl/multijob.groovy
+++ basics/dsl/multijob.groovy
@@ -16,3 +16,21 @@ job('NodeJS example2') {
         shell("npm install")
     }
 }
+job('NodeJS example3') {
+    scm {
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL ben')
+            node / gitConfigEmail('samesame@duck.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
